2f346126d98b3e8ebf4094a768eeb6406a4495c5
Mikhail Solodovnikov <miksolodovnikov@gmail.com>
2020-06-06 21:54:34 +0100

    Rebase from master. Small refactoring of TileMap after conflicts resoulution

--- src/components/TileMap/index.jsx
+++ src/components/TileMap/index.jsx
@@ -3,6 +3,7 @@ import * as d3 from 'd3';
 import styles from './TileMap.scss';
 import {
   getMapValues,
+  getValuesFx,
   selectedTile,
   setSelectedTile
 } from '../../utils/effector';
@@ -35,6 +36,14 @@ const TileMap = ({ mapWidth, mapHeight }) => {
     });
   }, []);
 
+  const [tileData, setTileData] = useState(false);
+  useEffect(() => {
+    getValuesFx({
+      range: 'Stat!B1:D86',
+      dateTimeRenderOption: 'SERIAL_NUMBER',
+      majorDimension: 'COLUMNS'
+    }).then(d => setTileData(d))}, []);
+
   useEffect(() => {
     selectedTile.watch(state => {
       document.querySelectorAll('[data-id]').forEach(el => {
@@ -51,7 +60,7 @@ const TileMap = ({ mapWidth, mapHeight }) => {
   const [view, setView] = useState({type: VIEW.map, order: ORDER.desc});
   useEffect(() => {
     if (data) buildTileMap();
-  }, [view, data]);
+  }, [view, data, tileData]);
 
   const calcX = (d, mC) => {
     if (view.type === VIEW.map) return d.col;
@@ -66,80 +75,76 @@ const TileMap = ({ mapWidth, mapHeight }) => {
   }
 
   const buildTileMap = () => {
-    getValuesFx({
-      range: 'Stat!B1:D86',
-      dateTimeRenderOption: 'SERIAL_NUMBER',
-      majorDimension: 'COLUMNS'
-    }).then(dataDocs => {
-      const maxColumns = d3.max(data, d => parseInt(d.col, 10));
-      const maxRows = d3.max(data, d => parseInt(d.row, 10));
+    const maxColumns = d3.max(data, d => parseInt(d.col, 10));
+    const maxRows = d3.max(data, d => parseInt(d.row, 10));
 
-      const tileWidth = mapWidth / (maxColumns + 1);
-      const tileHeight = mapHeight / (maxRows + 1);
+    const tileWidth = mapWidth / (maxColumns + 1);
+    const tileHeight = mapHeight / (maxRows + 1);
 
-      const svg = d3.select('#TileChart').append('svg');
-      svg.attr('width', chartWidth).attr('height', chartHeight);
-      svg.append('g').attr('id', 'tileArea');
+    d3.select('#TileChart').selectAll('*').remove();
+    const svg = d3.select('#TileChart').append('svg');
+    svg.attr('width', mapWidth).attr('height', mapHeight);
+    svg.append('g').attr('id', 'tileArea');
 
-      const tile = svg
-        .select('#tileArea')
-        .selectAll('g')
-        .data(data)
-        .enter()
-        .append('g')
-        .attr('data-id', d => d.id_reg)
-        .on("mouseover", function(d) {
-          let elements = document.getElementById('block');
-          elements.style.opacity = 1;
-          elements.style.left = d3.event.pageX + 10 + "px";
-          elements.style.top = d3.event.pageY + "px";
-          let elementRegion = document.getElementById('textRegion');
-          elementRegion.textContent = d.region;
-          let elementCount = document.getElementById('textCount');
-          elementCount.textContent = 'Количество случаев ' + dataDocs[0][dataDocs[1].indexOf(d.id_reg)];
-          let elementPopulation = document.getElementById('textPopulation');
-          elementPopulation.textContent = 'Население ' + dataDocs[2][dataDocs[1].indexOf(d.id_reg)];
-        })
-        .on("mouseout", function(d) {
-          let elements = document.getElementById('block');
-          elements.style.opacity = 0;
-          elements.style.left = window.innerWidth / 2 + "px";
-          elements.style.top = window.innerHeight / 2 + "px";
-        })
-        .on("click", (d) => {
-          setSelectedTile(d.id_reg)
-          history.push('/' + dataDocs[1].indexOf(d.id_reg))
-        })
-        .classed(styles['tile-map__tile-wrapper'], true);
+    const tile = svg
+      .select('#tileArea')
+      .selectAll('g')
+      .data(data)
+      .enter()
+      .append('g')
+      .attr('data-id', d => d.id_reg);
 
-      tile
-        .append('rect')
-        .style('fill', d => d.color)
-        .attr('width', tileWidth)
-        .attr('height', tileHeight)
-        .attr('x', d => calcX(d, maxColumns) * tileWidth)
-        .attr('y', d => calcY(d, maxColumns) * tileHeight)
-        .classed(styles['tile-map__tile'], true);
+    tile
+      .append('rect')
+      .style('fill', d => d.color)
+      .attr('width', tileWidth)
+      .attr('height', tileHeight)
+      .attr('x', d => calcX(d, maxColumns) * tileWidth)
+      .attr('y', d => calcY(d, maxColumns) * tileHeight)
+      .classed(styles['tile-map__tile'], true);
 
-      tile
-        .append('text')
-        .attr('x', d => calcX(d, maxColumns) * tileWidth + tileWidth / 5)
-        .attr('y', d => calcY(d, maxColumns) * tileHeight + tileHeight / 3)
-        .attr('dy', '.35em')
-        .text(d => d.region_rus)
-        .classed(styles['tile-map__caption'], true);
+    tile
+      .append('text')
+      .attr('x', d => calcX(d, maxColumns) * tileWidth + tileWidth / 5)
+      .attr('y', d => calcY(d, maxColumns) * tileHeight + tileHeight / 3)
+      .attr('dy', '.35em')
+      .text(d => d.region_rus)
+      .classed(styles['tile-map__caption'], true);
 
-      tile
-        .append('text')
-        .attr('x', d => calcX(d, maxColumns) * tileWidth + tileWidth / 5 + 10)
-        .attr('y', d => calcY(d, maxColumns) * tileHeight + tileHeight / 3 + 12)
-        .attr('dy', '.35em')
-        .text(d => d.value)
-        .classed(styles['tile-map__caption'], true);
+    tile
+      .append('text')
+      .attr('x', d => calcX(d, maxColumns) * tileWidth + tileWidth / 5 + 10)
+      .attr('y', d => calcY(d, maxColumns) * tileHeight + tileHeight / 3 + 12)
+      .attr('dy', '.35em')
+      .text(d => d.value)
+      .classed(styles['tile-map__caption'], true);
+
+    tile
+      .on("mouseover", function(d) {
+        let elements = document.getElementById('block');
+        elements.style.opacity = 1;
+        elements.style.left = d3.event.pageX + 10 + "px";
+        elements.style.top = d3.event.pageY + "px";
+        let elementRegion = document.getElementById('textRegion');
+        elementRegion.textContent = d.region;
+        let elementCount = document.getElementById('textCount');
+        elementCount.textContent = 'Количество случаев ' + tileData[0][tileData[1].indexOf(d.id_reg)];
+        let elementPopulation = document.getElementById('textPopulation');
+        elementPopulation.textContent = 'Население ' + tileData[2][tileData[1].indexOf(d.id_reg)];
+      })
+      .on("mouseout", function(d) {
+        let elements = document.getElementById('block');
+        elements.style.opacity = 0;
+        elements.style.left = window.innerWidth / 2 + "px";
+        elements.style.top = window.innerHeight / 2 + "px";
+      })
+      .on("click", (d) => {
+        setSelectedTile(d.id_reg)
+        history.push('/' + tileData[1].indexOf(d.id_reg))
+      }).classed(styles['tile-map__tile-wrapper'], true);
 
       return svg;
-    })
-  };
+  }
   
   if (!data) {
     return 'Загрузка...';
@@ -149,7 +154,7 @@ const TileMap = ({ mapWidth, mapHeight }) => {
     <div>
       <button onClick={() => setView({type: VIEW.tileChart, order: toggleOrder(view.order)})}>Sort by {view.order}</button>
       {view.type === VIEW.tileChart && <button onClick={() => setView({type: VIEW.map, order: ORDER.desc})}>Map View</button>}
-      <div id="TileChart"></div>
+      <div id="TileChart" />
       <div id='block' style={{left: 0, top: 0, opacity:0, position:'absolute', backgroundColor: 'rgb(108, 205, 216,0.7)'}}>
         <h3 id='textRegion'>1</h3>
         <h3 id='textCount'>1</h3>
